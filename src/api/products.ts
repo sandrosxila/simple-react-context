aceb364ca043d29fa803b69783761c3210e673d5
sandrosxila <sandrosxila@yahoo.com>
2025-02-19 17:03:16 +0400

    products pagination

--- src/api/products.ts
+++ src/api/products.ts
@@ -1,7 +1,16 @@
+import { Paginated } from '../models/api';
 import { Product } from '../models/product';
 
 export const getProducts = async (): Promise<Product[]> => {
   const response = await fetch('http://localhost:5000/products');
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+  return response.json();
+};
+
+export const getProductsByPage = async (page = 1): Promise<Paginated<Product>> => {
+  const response = await fetch(`http://localhost:5000/products?_page=${page}&_per_page=3`);
   if (!response.ok) {
     throw new Error('Network response was not ok');
   }
--- src/components/products.tsx
+++ src/components/products.tsx
@@ -2,10 +2,31 @@ import { useQuery } from "@tanstack/react-query";
 import { useShop } from "../hooks/useShop";
 import styles from "./products.module.css";
 import { Product } from "../models/product";
+import { Paginated } from "../models/api";
+import {getProductsByPage} from '../api/products';
+import { useState } from "react";
+
+const DEFAULT_PAGINATION_STATE : Paginated<Product> = {
+  first: 0,
+  prev: 0,
+  next: 0,
+  last: 0,
+  pages: 0,
+  items: 0,
+  data: []
+}
 
 export const Products = () => {
   const { addToCart } = useShop();
-  const { data: products = [], error, isLoading } = useQuery<Product[]>({queryKey: ['products']});
+
+  const [currentPage, setCurrentPage] = useState(1);
+  
+  const { data = DEFAULT_PAGINATION_STATE, error, isLoading } = useQuery<Paginated<Product>>({
+    queryFn: () => getProductsByPage(currentPage),
+    queryKey: ['products', currentPage]
+  });
+
+  const { data : products, prev, next, last, pages} = data;
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error: {error.message}</div>;
@@ -21,6 +42,25 @@ export const Products = () => {
           </li>
         ))}
       </ul>
+      <div className={styles["pagination"]}>
+        <button
+          className={styles.button}
+          onClick={() => setCurrentPage(prev)}
+          disabled={currentPage === 1}
+        >
+          Previous
+        </button>
+        <span className={styles["pagination-info"]}>
+          Page {currentPage} of {pages}
+        </span>
+        <button
+          className={styles.button}
+          onClick={() => setCurrentPage(next)}
+          disabled={currentPage === last}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 };
